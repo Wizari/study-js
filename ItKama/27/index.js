b22dcc6cf1b019f5d23e9b62e072ed3a69c2e9c9
Wizari <wizaripost@gmail.com>
2021-10-06 19:23:45 +0300

    28

---
+++ ItKama/27/index.js
@@ -0,0 +1,19 @@
+var slider1 = new Slider();
+var slider2 = new Slider();
+
+slider1.start("w-slider1");
+slider2.start("w-slider2");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ItKama/28/index.js
@@ -0,0 +1,22 @@
+// var slider1 = new Slider();
+// var slider2 = new Slider();
+//
+// slider1.start("w-slider1");
+// slider2.start("w-slider2");
+
+var reno = new Car("Reno");
+reno.start();
+var mers = new Car("Mers");
+mers.start();
+
+
+
+
+
+
+
+
+
+
+
+
